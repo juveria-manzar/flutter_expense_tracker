9882c613637a612b267b414172ac5af4982b87d5
Juveria Manzar <juveriadot@gmail.com>
2020-04-27 08:23:37 +0530

    adding-combined-widgets

--- lib/main.dart
+++ lib/main.dart
@@ -13,17 +13,27 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  Widget build(BuildContext context) {}
-}
-
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: Text('Flutter App'),
-    ),
-    body: Center(
-      child: Text('Web Developer'),
-    ),
-  );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('Chart'),
+              elevation: 5,
+            ),
+          ),
+          Card(
+            child: Text('Expense List'),
+          )
+        ],
+      ),
+    );
+  }
 }
